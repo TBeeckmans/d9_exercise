6ead6dafa1b6f41d95bc44e13dcfe609ad9ecaec
Thierry Beeckmans <thierry@dropsolid.com>
2021-11-20 23:48:07 +0100

    Added entity basics for the best_price module

---
+++ web/modules/custom/best_price/src/Entity/BestPriceEntity.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\best_price\Entity;
+
+use Drupal\Core\Entity\ContentEntityBase;
+
+/**
+ * Defines the Best Price entity.
+ *
+ * @ContentEntityType(
+ *   id = "best_price",
+ *   label = @Translation("Best Price"),
+ *   base_table = "best_price",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "bundle" = "bundle",
+ *   },
+ *   bundle_entity_type = "best_price_type",
+ *   handlers = {
+ *     "view_builder" = "Drupal\Core\Entity\EntityViewBuilder",
+ *     "list_builder" = "Drupal\Core\Entity\EntityListBuilder",
+ *     "views_data" = "Drupal\views\EntityViewsData",
+ *     "access" = "Drupal\Core\Entity\EntityAccessControlHandler",
+ *     "form" = {
+ *       "default" = "Drupal\Core\Entity\ContentEntityForm",
+ *       "add" = "Drupal\Core\Entity\ContentEntityForm",
+ *       "edit" = "Drupal\Core\Entity\ContentEntityForm",
+ *       "delete" = "Drupal\Core\Entity\ContentEntityDeleteForm",
+ *     },
+ *     "route_provider" = {
+ *       "html" = "Drupal\Core\Entity\Routing\AdminHtmlRouteProvider",
+ *     },
+ *   },
+ *   links = {
+ *     "canonical" = "/best_price/{best_price}",
+ *     "add-page" = "/best_price/add",
+ *     "add-form" = "/best_price/add/{best_price_type}",
+ *     "edit-form" = "/best_price/{best_price}/edit",
+ *     "delete-form" = "/best_price/{best_price}/delete",
+ *     "collection" = "/admin/content/best_prices",
+ *   },
+ *   admin_permission = "administer best_price types",
+ *   fieldable = TRUE,
+ *   field_ui_base_route = "entity.best_price.edit_form",
+ * )
+ */
+class BestPriceEntity extends ContentEntityBase {
+
+}
---
+++ web/modules/custom/best_price/src/Entity/BestPriceTypeEntity.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Drupal\best_price\Entity;
+
+use Drupal\Core\Config\Entity\ConfigEntityBundleBase;
+
+/**
+ * Defines the Best Price Type entity.
+ *
+ * A configuration entity used to manage bundles for the Best Price entity.
+ *
+ * @ConfigEntityType(
+ *   id = "best_price_type",
+ *   label = @Translation("Best Price Type"),
+ *   bundle_of = "best_price",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "label",
+ *     "uuid" = "uuid",
+ *   },
+ *   config_prefix = "best_price_type",
+ *   config_export = {
+ *     "id",
+ *     "label",
+ *   },
+ *   handlers = {
+ *     "view_builder" = "Drupal\Core\Entity\EntityViewBuilder",
+ *     "list_builder" = "Drupal\Core\Entity\EntityListBuilder",
+ *     "form" = {
+ *       "default" = "Drupal\best_price\Form\BestPriceTypeEntityForm",
+ *       "add" = "Drupal\best_price\Form\BestPriceTypeEntityForm",
+ *       "edit" = "Drupal\best_price\Form\BestPriceTypeEntityForm",
+ *       "delete" = "Drupal\Core\Entity\EntityDeleteForm",
+ *     },
+ *     "route_provider" = {
+ *       "html" = "Drupal\Core\Entity\Routing\AdminHtmlRouteProvider",
+ *     },
+ *   },
+ *   links = {
+ *     "canonical" = "/admin/structure/best_price_type/{best_price_type}",
+ *     "add-form" = "/admin/structure/best_price_type/add",
+ *     "edit-form" = "/admin/structure/best_price_type/{best_price_type}/edit",
+ *     "delete-form" = "/admin/structure/best_price_type/{best_price_type}/delete",
+ *     "collection" = "/admin/structure/best_price_type",
+ *   },
+ *   admin_permission = "administer best_price types",
+ * )
+ */
+class BestPriceTypeEntity extends ConfigEntityBundleBase {
+
+}
