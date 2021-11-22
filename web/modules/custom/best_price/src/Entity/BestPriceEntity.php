e9f640a47398cddd1b8427de0a08f23d1218fdde
Thierry Beeckmans <thierry@dropsolid.com>
2021-11-22 21:39:16 +0100

    Added field to Best Price entity

--- web/modules/custom/best_price/src/Entity/BestPriceEntity.php
+++ web/modules/custom/best_price/src/Entity/BestPriceEntity.php
@@ -3,6 +3,8 @@
 namespace Drupal\best_price\Entity;
 
 use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
 
 /**
  * Defines the Best Price entity.
@@ -14,6 +16,12 @@ use Drupal\Core\Entity\ContentEntityBase;
  *   entity_keys = {
  *     "id" = "id",
  *     "bundle" = "bundle",
+ *     "uid" = "uid",
+ *     "label" = "name",
+ *     "created" = "created",
+ *     "changed" = "changed",
+ *     "price" = "price",
+ *     "store" = "store",
  *   },
  *   bundle_entity_type = "best_price_type",
  *   handlers = {
@@ -46,4 +54,103 @@ use Drupal\Core\Entity\ContentEntityBase;
  */
 class BestPriceEntity extends ContentEntityBase {
 
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+    $fields = parent::baseFieldDefinitions($entity_type);
+
+    $fields['uid'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Authored by'))
+      ->setDescription(t('The user ID of author of the Best Peice entity.'))
+      ->setSetting('target_type', 'user')
+      ->setSetting('handler', 'default')
+      ->setDisplayOptions('view', [
+        'label' => 'hidden',
+        'type' => 'author',
+        'weight' => 0,
+      ])
+      ->setDisplayOptions('form', [
+        'type' => 'entity_reference_autocomplete',
+        'weight' => 5,
+        'settings' => [
+          'match_operator' => 'CONTAINS',
+          'size' => '60',
+          'autocomplete_type' => 'tags',
+          'placeholder' => '',
+        ],
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['name'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Name'))
+      ->setDescription(t('The name of the Best Price entity.'))
+      ->setSettings([
+        'max_length' => 50,
+        'text_processing' => 0,
+      ])
+      ->setDefaultValue('')
+      ->setDisplayOptions('view', [
+        'label' => 'hidden',
+        'type' => 'string',
+        'weight' => -4,
+      ])
+      ->setDisplayOptions('form', [
+        'type' => 'string_textfield',
+        'weight' => -4,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE)
+      ->setRequired(TRUE);
+
+    $fields['store'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Store'))
+      ->setDescription(t('The name of the store.'))
+      ->setSettings([
+        'max_length' => 50,
+        'text_processing' => 0,
+      ])
+      ->setDefaultValue('')
+      ->setDisplayOptions('view', [
+        'label' => 'hidden',
+        'type' => 'string',
+        'weight' => -3,
+      ])
+      ->setDisplayOptions('form', [
+        'type' => 'string_textfield',
+        'weight' => -3,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE)
+      ->setRequired(TRUE);
+
+    $fields['price'] = BaseFieldDefinition::create('float')
+      ->setLabel(t('Price'))
+      ->setDescription(t('Price of the Currency'))
+      ->setDisplayOptions('view', [
+        'label' => 'hidden',
+        'type' => 'number_decimal',
+        'weight' => -3,
+      ])
+      ->setDisplayOptions('form', [
+        'type' => 'number',
+        'weight' => -3,
+        'settings' => [
+          'display_label' => TRUE,
+        ],
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setRequired(TRUE);
+
+    $fields['created'] = BaseFieldDefinition::create('created')
+      ->setLabel(t('Created'))
+      ->setDescription(t('The time that the entity was created.'));
+
+    $fields['changed'] = BaseFieldDefinition::create('changed')
+      ->setLabel(t('Changed'))
+      ->setDescription(t('The time that the entity was last edited.'));
+
+    return $fields;
+  }
 }
